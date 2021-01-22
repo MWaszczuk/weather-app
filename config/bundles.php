24943ab81284df124bd401a42cb7458d6d43e5cf
Mateusz Waszczuk <mateusz.waszczuk@itligent.pl>
2021-01-22 17:00:41 +0100

    Deploy update

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Knp\DoctrineBehaviors\DoctrineBehaviorsBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyDeployBundle\EasyDeployBundle::class => ['dev' => true, 'test' => true],
 ];
---
+++ config/prod/deploy.php
@@ -0,0 +1,29 @@
+<?php
+
+use EasyCorp\Bundle\EasyDeployBundle\Deployer\DefaultDeployer;
+
+return new class extends DefaultDeployer
+{
+    public function configure()
+    {
+        return $this->getConfigBuilder()
+            ->server('my-vps')
+            ->deployDir('/var/www/weather-app')
+            ->repositoryUrl('git@github.com:MWaszczuk/weather-app.git')
+            ->repositoryBranch('master')
+        ;
+    }
+
+    // run some local or remote commands before the deployment is started
+    public function beforeStartingDeploy()
+    {
+        // $this->runLocal('./vendor/bin/simple-phpunit');
+    }
+
+    // run some local or remote commands after the deployment is finished
+    public function beforeFinishingDeploy()
+    {
+        // $this->runRemote('{{ console_bin }} app:my-task-name');
+        // $this->runLocal('say "The deployment has finished."');
+    }
+};
